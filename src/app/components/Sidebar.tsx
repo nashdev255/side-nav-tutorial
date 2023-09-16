3114b934ae743469732f95b694225e7be46e1bca
nashdev255 <yutori1107@gmail.com>
2023-09-16 20:23:22 +0900

    yatta-

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -32,25 +32,29 @@ const navigation = [
   },
 ];
 
-const Sidebar = () => {
+const Sidebar = ({children}: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
   return (
-    <div className='max-w-xl grid grid-cols-12 mx-auto'>
-      <div className='text-sm space-y-1 font-bold md:col-span-4'>
+    <div className='mx-auto flex max-w-2xl'>
+      <div className='mt-12 space-y-1 text-sm font-bold'>
         {navigation.map((item, index) => (
           <Link href={item.href} key={index}>
-            <div className={`${ item.href == pathname && 'bg-sky-100 text-sky-500'} hover:bg-sky-100 px-3 py-2 rounded-full md:flex`}>
-              <div className='flex'>
-                <item.icon className='w-10 h-10' />
-                <div className='pl-3 flex items-center'>{item.name}</div>
+            <div className={`
+            ${item.href == pathname && 'bg-sky-100 text-sky-500'}
+            flex rounded-full hover:bg-sky-100`}
+            >
+              <item.icon className='m-2 h-10 w-10' />
+              <div className='flex w-0 items-center text-[0] md:w-32 md:text-sm'>
+                {item.name}
               </div>
             </div>
           </Link>
         ))}
       </div>
+      <div>{children}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;
