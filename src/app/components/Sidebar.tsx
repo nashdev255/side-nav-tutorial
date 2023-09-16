55355bb55c8729ee30da98fd92b5a8528ff50a2f
nashdev255 <yutori1107@gmail.com>
2023-09-16 19:24:28 +0900

    pc resp

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,28 +1,54 @@
+'use client'
+
 import {
-  HomeIcon,
-  ChatBubbleLeftRightIcon
-} from '@heroicons/react/24/solid'
+  UserCircleIcon,
+  EnvelopeIcon,
+  KeyIcon,
+  ArrowLeftOnRectangleIcon
+} from '@heroicons/react/24/outline';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const navigation = [
   {
-    name: 'Home',
-    icon: HomeIcon,
-    link: '/home'
+    name: 'プロフィール',
+    icon: UserCircleIcon,
+    href: '/settings/profile',
   },
   {
-    name: 'Chat',
-    icon: ChatBubbleLeftRightIcon,
-    link: '/chat'
-  }
-]
-
-const nav = navigation[0];
+    name: 'メールアドレス',
+    icon: EnvelopeIcon,
+    href: '/settings/email',
+  },
+  {
+    name: 'パスワード',
+    icon: KeyIcon,
+    href: '/settings/password',
+  },
+  {
+    name: 'ログアウト',
+    icon: ArrowLeftOnRectangleIcon,
+    href: '/settings/logout',
+  },
+];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <div className='p-2 bg-slate-700 text-white flex'>
-      <nav.icon className='h-8 w-8' />
-      <h3 className='mx-6 flex items-center'>{nav.name}</h3>
+    <div className='max-w-xl grid grid-cols-12 mx-auto'>
+      <div className='text-sm space-y-1 font-bold md:col-span-4'>
+        {navigation.map((item, index) => (
+          <Link href={item.href} key={index}>
+            <div className={`${ item.href == pathname && 'bg-sky-100 text-sky-500'} hover:bg-sky-100 px-3 py-2 rounded-full md:flex`}>
+              <div className='flex'>
+                <item.icon className='w-10 h-10' />
+                <div className='pl-3 flex items-center'>{item.name}</div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
